7a32a2f214d34833f16858361aded15339aac219
fitsum-kebede <fitsuminfome@gmail.com>
2023-07-25 16:42:11 -0700

    SOrting Algorithms

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -1,39 +1,46 @@
 #include "sort.h"
 
 /**
- * bubble_sort - compares adjacent elements and swaps them
- * @array: array
- * @size: size of the array
- * Return: Void
+ * swap_ints - Swap two integers in an array.
+ * @a: The first integer to swap.
+ * @b: The second integer to swap.
+ */
+void swap_ints(int *a, int *b)
+{
+	int tmp;
+
+	tmp = *a;
+	*a = *b;
+	*b = tmp;
+}
+
+/**
+ * bubble_sort - Sort an array of integers in ascending order.
+ * @array: An array of integers to sort.
+ * @size: The size of the array.
+ *
+ * Description: Prints the array after each swap.
  */
 void bubble_sort(int *array, size_t size)
 {
-	int swap;
-	size_t i, j;
+	size_t i, len = size;
+	bool bubbly = false;
 
-	if (size < 2)
-	{
+	if (array == NULL || size < 2)
 		return;
-	}
 
-
-	for (i = 0; i <= size; i++)
+	while (bubbly == false)
 	{
-		if (array[i + 1])
+		bubbly = true;
+		for (i = 0; i < len - 1; i++)
 		{
-			for (j = 0; j <= size; j++)
+			if (array[i] > array[i + 1])
 			{
-				if (array[j + 1])
-				{
-					if (array[j] > array[j + 1])
-					{
-						swap = array[j];
-						array[j] = array[j + 1];
-						array[j + 1] = swap;
-						print_array(array, size);
-					}
-				}
+				swap_ints(array + i, array + i + 1);
+				print_array(array, size);
+				bubbly = false;
 			}
 		}
+		len--;
 	}
 }
